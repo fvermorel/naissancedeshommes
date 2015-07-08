77107fa57f0dc3cefdeeb648b349b40e45255f7d
fvermorel <fvermorel@gmail.com>
2015-07-08 10:37:16 +0200

    Update script1.js

--- js/script1.js
+++ js/script1.js
@@ -8,28 +8,7 @@
 	   "resources/LNDH_004.mp4",
 	   "resources/LNDH_005.mp4",
 	   
-	   "resources/LNDH_006.mp4",
-	   "resources/LNDH_007.mp4",
-	   "resources/LNDH_008.mp4",
-	   "resources/LNDH_009.mp4",
-	   "resources/LNDH_0010.mp4",
-	   
-	   "resources/LNDH_0011.mp4",
-	   "resources/LNDH_0012.mp4",
-	   "resources/LNDH_0013.mp4",
-	   "resources/LNDH_0014.mp4",
-	   "resources/LNDH_0015.mp4",
-	   
-	   "resources/LNDH_0016.mp4",
-	   "resources/LNDH_0017.mp4",
-	   "resources/LNDH_0018.mp4",
-	   "resources/LNDH_0019.mp4",
-	   "resources/LNDH_0020.mp4",
-	   
-	   "resources/LNDH_0021.mp4",
-	   "resources/LNDH_0022.mp4",
-	   "resources/LNDH_0023.mp4",
-	   "resources/LNDH_0024.mp4"
+	 
    
     ], function() {
         startApp();
